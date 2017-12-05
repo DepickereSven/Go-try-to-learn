1255da89aa05118cdbf68a9d21a1461421086e61
svend <sven.depickere@student.howest.be>
2017-12-05 20:03:54 +0100

    Update Hello world!

--- Hello-world/Hello-World.go
+++ Hello-world/Hello-World.go
@@ -1,5 +1,27 @@
-package Hello_world
+package main
+
+//if you don't use the import it will not work
+import (
+	"fmt"
+)
+
+//can't be changed
+const (
+	Stringmessage = "Second Hello world!"
+)
+
+//just a simple var
+var (
+	messageForWorld string
+)
 
 func main() {
-	println("hello, world\n")
+	fmt.Println("Hello World!")
+	fmt.Println(Stringmessage)
+	fmt.Println(messageForWorld)
+}
+
+
+func init() {
+	messageForWorld = "New hello World!"
 }
